4ef20b3439c1465243ee8ba01658441c8247ce15
Vinod <37751567+vinodvidhole@users.noreply.github.com>
2022-02-23 21:55:20 +0000

    push

--- scraper.py
+++ scraper.py
@@ -19,17 +19,33 @@ if __name__ == "__main__":
 
   print('Fetching the page')
   driver.get(YAHOO_FINANCE_URL)
-  print('Page title:', driver.title)
-
-  #TICKER_TABLE_CLASS = "W(100%)"
-  
-  #ticker_table  = driver.find_element_by_class_name(TICKER_TABLE_CLASS)
-  #ticker_table = driver.find_element(By.CLASS_NAME, TICKER_TABLE_CLASS)
-
-  for row in driver.find_elements(By.XPATH, value='//table[@class="W(100%)"]//tr'):
-    #print(row.get_attribute('id'))
-    print(row.text)
     
-  #print(f'Found {len(ticker_table)} table')
+  #VALUE_EXPRESSION='//table[@class="W(100%)"]//tr' for row count
+  TABLE_CLASS = "W(100%)"
+  ROW = 3
+  EXPRESSION_ROWCOUNT = "//table[@class= '{}']/tbody/tr".format(TABLE_CLASS)
+  EXPRESSION_COLUMN = "//*[@class= '{}']/tbody/tr[{}]/td".format(TABLE_CLASS,ROW)
+  #VALUE_EXPRESSION = "//*[@class= '{}']/tbody/tr[1]/th".format(TABLE_CLASS,ROW)
+   
+  # to identify the table rows
+  r = driver.find_elements(By.XPATH, value="//table[@class= '{}']/tbody/tr".format(TABLE_CLASS))
+  # to identify table columns
+  c = driver.find_elements(By.XPATH, value="//*[@class= '{}']/tbody/tr[1]/td".format(TABLE_CLASS))
+  # to get row count with len method
+  rc = len (r)
+  print('rc ', rc)
+  
+  # to get column count with len method
+  cc = len (c)
+  print('c ', cc)
+  
+  # to traverse through the table rows excluding headers
+  for i in range (2, rc + 1) :
+    # to traverse through the table column
+    for j in range (1, cc + 1) :
+    # to get all the cell data with text method
+      d = driver.find_element(By.XPATH, value="//tr["+str(i)+"]/td["+str(j)+"]").text
+      print(j)
+      print (d)
 
 print("End")
