837bd7c23a8943943342969633b332766db05564
Vinod <37751567+vinodvidhole@users.noreply.github.com>
2022-02-23 19:10:08 +0000

    check in latest version

--- scraper.py
+++ scraper.py
@@ -1,1 +1,11 @@
-print("hello word")
+import requests
+
+YAHOO_FINANCE_URL = 'https://finance.yahoo.com/trending-tickers'
+
+response = requests.get(YAHOO_FINANCE_URL)
+
+print('Status Code ', response.status_code)
+
+with open('trending-tickers.html','w') as f:
+  f.write(response.text)
+  
